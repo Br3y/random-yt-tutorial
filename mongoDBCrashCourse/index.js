99b361cd5d0676e8c2e7716c629d59c4239cdcdd
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-11-06 23:03:27 +0800

    travsMongoCrashCourse22

--- mongoDBCrashCourse/index.js
+++ mongoDBCrashCourse/index.js
@@ -1,3 +1,5 @@
+db.createCollection("posts");
+
 db.posts.insertOne({
     title: "Post 1",
     body: "Body of post.",
@@ -12,4 +14,31 @@ db.posts.find()
 db.posts.insertMany()
 
 db.posts.find({name: "Aubrey"}).count()
+
 db.posts.sort()
+
+db.posts.find().limit(2)
+
+db.posts.findOne() //return first match
+
+$gt, $lt, $lte, $gte
+
+updateOne({title: "{Post 1"}, {likes: 2})
+
+$set
+
+$upset //update if found
+
+$inc // increment
+
+updateMany({})
+
+db.sales.drop() // delete collection
+
+aggregate()
+
+close()
+
+$limit, $match
+
+toArray()

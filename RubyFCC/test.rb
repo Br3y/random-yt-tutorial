aabbdad2146398af0fd2bc121d581c3e454a6f7c
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-21 01:39:27 +0800

    Introduction to Array

---
+++ RubyFCC/test.rb
@@ -0,0 +1,14 @@
+# Arrays
+friends = Array["Kevin", "Karen", "Oscar"]
+puts friends[0]
+puts friends[0]
+puts "#{friends[0,2]}"
+
+new_friends = Array.new
+new_friends[0] = "Michael"
+new_friends[5] = "Holly"
+puts new_friends
+
+# methods
+# .length, .include?, .reverse, .sort
+# you cant sort string and number together inside array

7caf471ed057b582ff20eda8f6a0177ef96bbbf4
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-12-21 01:49:30 +0800

    Introduction to Array

--- RubyFCC/test.rb
+++ RubyFCC/test.rb
@@ -12,3 +12,20 @@ puts new_friends
 # methods
 # .length, .include?, .reverse, .sort
 # you cant sort string and number together inside array
+
+
+=begin
+    hashes - like dictionaries that have ( key => value )
+=end
+puts "--------------------------------------"
+states = {
+    "Pennsylvania" => "PA",
+    "New York" => "NY",
+    "Oregon" => "OR",
+    :Philippines => "PH",
+    1 => "One"
+}
+puts states
+puts states["New York"]
+puts states[:Philippines]
+puts states[1]

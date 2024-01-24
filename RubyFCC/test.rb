96872ee3315b01cd32a1e406faff9bb770f40fe3
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-01-25 01:51:28 +0800

    learn: for loops and iteration

--- RubyFCC/test.rb
+++ RubyFCC/test.rb
@@ -126,3 +126,20 @@ while index <= 5
     puts index
     index += 1
 end  
+
+
+# For Loop
+puts "--------------------------------------"
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+for friend in friends
+   puts friend 
+end
+friends.each do |friend|
+    puts friend
+end
+for index in 0..5
+    puts index
+end
+6.times do |index|
+    puts index
+end

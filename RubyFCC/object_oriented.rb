9090c95cda195e009a0c02e5c45e9641317d5217
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-06 14:25:43 +0800

    learn: initialize method to optimize code and less line

--- RubyFCC/object_oriented.rb
+++ RubyFCC/object_oriented.rb
@@ -1,12 +1,16 @@
 # Class and Objects
 class Book
   attr_accessor :title, :author, :pages
+
+  # initialize method
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
 end
-book1 = Book.new() # Object
-book1.title = "Harry Potter"
-book1.author = "Jk Rowling"
-book1.pages = 400
-puts book1.title
-puts book1.author
-puts book1.pages
+book1 = Book.new("Harry Potter", "Jk Rowling", 400) # Object
 p book1
+
+book2 = Book.new("Lord of the Rings", "Tolkien", 500) # when use initialize method
+p book2

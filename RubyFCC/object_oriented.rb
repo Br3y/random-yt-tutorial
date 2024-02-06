3cc6f0a514f4d4d81aaae4ff4856f99bf23236fc
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-02-06 22:10:06 +0800

    learn: inheritance to classes

--- RubyFCC/object_oriented.rb
+++ RubyFCC/object_oriented.rb
@@ -35,3 +35,38 @@ student1 = Student.new("Jim", "Business", 2.6)
 student2 = Student.new("Pam", "Art", 3.6)
 puts student1.has_honors
 puts student2.has_honors
+
+
+# Inheritance
+puts "--------------------------------------"
+class Chef
+  def make_chicken
+    puts "The chef makes chicken"
+  end
+
+  def make_salad
+    puts "The chef makes salad"
+  end
+
+  def make_special_dish
+    puts "The chef makes bbq dibs"
+  end
+end
+
+class ItalianChef < Chef
+  def make_special_dish
+    puts "The chef makes eggplant parn"
+  end
+  
+  def make_pasta
+    puts "The chef makes pasta"
+  end
+end
+
+chef = Chef.new
+chef.make_chicken
+
+italian_chef = ItalianChef.new
+italian_chef.make_salad
+italian_chef.make_special_dish
+italian_chef.make_pasta

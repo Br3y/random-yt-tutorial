2de8edb0322052d5b89647df84ca95d914076b54
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-04-23 12:47:00 +0800

    variables in javascript

--- BroCode/JavaScript/index.js
+++ BroCode/JavaScript/index.js
@@ -15,3 +15,40 @@
   multiline
   comment
 */
+
+
+// variable = A container that stores a value. Behaves as if it were the value it contains
+// 1. declaration let x 
+// 2. assignment x = 100
+
+// let x = 100
+// // let age = 25
+// let price = 10.99
+// let gpa = 2.1
+
+// console.log(typeof price)
+
+// let firstName = 'br3y'
+// let favoriteFood = "pizza"
+// let email = "br3y@gmail.com"
+
+// console.log(typeof firstName)
+// console.log(`Your name is ${firstName}`)
+// console.log(`Your like is ${favoriteFood}`)
+// console.log(`Your email is ${email}`)
+
+// let online = true
+// let forSale = false
+// let isStudent = true
+
+// console.log(`Bro is online: ${online}`)
+// console.log(`Is this car for sale: ${forSale}`)
+// console.log(`Enrolled: ${isStudent}`)
+
+// let fullName = "Aubrey Llanes Lizardo"
+// let age = 25
+// let student = true
+
+// document.querySelector("#p1").textContent = "my name is " + fullName
+// document.getElementById("p2").textContent = "my age is " + age
+// document.getElementById("p3").textContent = `Enrolled: ${student}`

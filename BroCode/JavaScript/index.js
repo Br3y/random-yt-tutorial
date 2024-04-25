9ca87424ba42287bee83b3d0fe868564ba9997be
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-04-25 14:31:21 +0800

    learn about constants

--- BroCode/JavaScript/index.js
+++ BroCode/JavaScript/index.js
@@ -129,3 +129,28 @@
 // console.log(x, typeof x)
 // console.log(y, typeof y)
 // console.log(z, typeof z)
+
+
+
+// // const = a variable that can't be changed
+
+// const PI = 3.14159;
+// // let radius = document.getElementById("myText");
+// let circumference;
+
+// // radius = window.prompt('Enter the radius of a circle')
+// // radius = Number(radius)
+// // document.getElementById("mySubmit").addEventListener("click", ()=>{
+  
+// //   circumference = 2 * PI * radius.value
+  
+// //   console.log(circumference)
+// // })
+
+// document.getElementById("mySubmit").onclick = function(){
+//   radius = document.getElementById("myText").value
+//   radius = Number(radius)
+//   circumference = 2 * PI * radius
+//   console.log(circumference)
+//   document.getElementById("myh3").textContent += circumference
+// }
